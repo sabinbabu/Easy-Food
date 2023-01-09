27aaef12270f86701e34f395422efda26c761d3e
Sabin Acharya <sabinacharya444@gmail.com>
2023-01-09 23:55:55 +1100

    Using custom scope instead of viewModel Scope

--- app/src/main/java/com/binwin/easyfood/ui/meals/MealsCategoryViewModel.kt
+++ app/src/main/java/com/binwin/easyfood/ui/meals/MealsCategoryViewModel.kt
@@ -2,28 +2,35 @@ package com.binwin.easyfood.ui.meals
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.binwin.easyfood.model.MealsRepository
 import com.binwin.easyfood.model.response.MealResponse
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MealsCategoryViewModel(
     private val repository: MealsRepository = MealsRepository()
 ): ViewModel() {
 
-    
+    //creating custom scope instead of viewModel Scope
+    private val mealsJob = Job()
 
     init {
-        viewModelScope.launch(Dispatchers.IO) {
+        val scope = CoroutineScope(mealsJob+Dispatchers.IO)
+        scope.launch(Dispatchers.IO) {
             val meals = getMeals()
             mealsState.value = meals
         }
     }
 
     val mealsState : MutableState<List<MealResponse>> =  mutableStateOf(emptyList())
+
+    override fun onCleared() { //triggered when view model is destroyed
+        super.onCleared()
+        mealsJob.cancel()
+    }
 
     private suspend fun getMeals() : List<MealResponse>{
         return repository.getMeals().categories
