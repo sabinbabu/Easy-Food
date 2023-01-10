d299c4a5583fa0391b58240f08eaf343d98b947c
Sabin Acharya <sabinacharya444@gmail.com>
2023-01-10 22:42:25 +1100

    Meals category screen created

--- app/src/main/java/com/binwin/easyfood/ui/meals/MainActivity.kt
+++ app/src/main/java/com/binwin/easyfood/ui/meals/MainActivity.kt
@@ -3,16 +3,7 @@ package com.binwin.easyfood.ui.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.binwin.easyfood.model.response.MealResponse
 import com.binwin.easyfood.ui.theme.EasyFoodTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,23 +14,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-
-@Composable
-fun MealsCategoryScreen() {
-   val viewModel : MealsCategoryViewModel = viewModel()
-    val meals = viewModel.mealsState.value
-    //lets things inside launched effect called only once
-    LazyColumn {
-        items(meals){meal ->
-            Text(text = meal.name)
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MealsCategoryScreen()
 }
--- app/src/main/java/com/binwin/easyfood/ui/meals/MealsCategoryViewModel.kt
+++ app/src/main/java/com/binwin/easyfood/ui/meals/MealsCategoryViewModel.kt
@@ -3,23 +3,19 @@ package com.binwin.easyfood.ui.meals
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.binwin.easyfood.model.MealsRepository
 import com.binwin.easyfood.model.response.MealResponse
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MealsCategoryViewModel(
     private val repository: MealsRepository = MealsRepository()
 ): ViewModel() {
 
-    //creating custom scope instead of viewModel Scope
-    private val mealsJob = Job()
-
     init {
-        val scope = CoroutineScope(mealsJob+Dispatchers.IO)
-        scope.launch(Dispatchers.IO) {
+
+        viewModelScope.launch(Dispatchers.IO) {
             val meals = getMeals()
             mealsState.value = meals
         }
@@ -27,10 +23,6 @@ class MealsCategoryViewModel(
 
     val mealsState : MutableState<List<MealResponse>> =  mutableStateOf(emptyList())
 
-    override fun onCleared() { //triggered when view model is destroyed
-        super.onCleared()
-        mealsJob.cancel()
-    }
 
     private suspend fun getMeals() : List<MealResponse>{
         return repository.getMeals().categories
